1375c65adddf6e563a812553056bc25bf6a0e246
PabloMarhoff <marhoff@campus.tu-berlin.de>
2018-11-11 19:31:51 +0000

    Hardware-Interrupts aktiviert aber laufen noch nicht

--- src/bs-logic/Interrupt_Handler.c
+++ src/bs-logic/Interrupt_Handler.c
@@ -1,11 +1,38 @@
 #include "../include/Interrupt_Handler.h"
 #include "../include/kprintf.h"
-#define TIMER_BASE 0x7E00B000 //TODO: kein Range??
+
+/** ab hier defines für timer*/
+#define TIMER_BASE (0x7E00B000 - 0x3F000000) //timerbaseadress minus MMU-offset
+#define IRQ_FIQ_REG_OFFSET 0x200
+#define TIMER_EN_SHIFT 7
+#define TIMER_INTERRUPT_EN_SHIFT 5
+#define TIMER_PRESCALE_MSB_SHIFT 3
+#define TIMER_PRESCALE_LSB_SHIFT 2
+#define TIMER_32BIT_COUNTER 1
+#define ARM_TIMER 0
+#define PREDIVIDER_VALUE 42 //1023 /* 250MHz -> Prescale -> Predivider -> 954,6 Hz*/
+#define TIMER_VALUE 10//954
 
 
 static volatile
 struct timer* const watchdog_reg = (struct timer*) TIMER_BASE;
 
+/* Setzt/-erneut watchdog-timer auf 1 sec mit Interrupt */
+void watchdog() {
+    watchdog_reg->LOAD = TIMER_VALUE;
+    watchdog_reg->CONTROL |= ((1 << TIMER_INTERRUPT_EN_SHIFT) | (1 << TIMER_PRESCALE_MSB_SHIFT) | (1 << TIMER_32BIT_COUNTER));
+    watchdog_reg->CONTROL &= ~(1 << TIMER_PRESCALE_LSB_SHIFT);
+    watchdog_reg->PREDIVIDER = PREDIVIDER_VALUE;
+    watchdog_reg->CONTROL = (1 << TIMER_EN_SHIFT); /* Timer starten */
+};
+
+static volatile
+struct en_basic_irq* const en_basic_irq_reg = (struct en_basic_irq*) (TIMER_BASE + IRQ_FIQ_REG_OFFSET);
+
+/* aktiviert Timer- und UART-Interrupts*/
+void enable_ext_interrupts () {
+    en_basic_irq_reg->EN_BASIC_IRQS |= (1 << ARM_TIMER);
+}
 
 void print_interrupt(uint32_t stackadress, uint32_t cpsr, uint32_t spsr, char* interrupt_name){
     kprintf("\n\r");
@@ -32,6 +59,11 @@ void print_interrupt(uint32_t stackadress, uint32_t cpsr, uint32_t spsr, char* i
     kprintf("\n\r");
 }
 
+void watchdog_init() {
+    //set timer
+    //
+}
+
 
 void reset(uint32_t stackadress, uint32_t cpsr, uint32_t spsr) {
     print_interrupt(stackadress, cpsr, spsr, "RESET");
@@ -48,4 +80,9 @@ void prefab(uint32_t stackadress, uint32_t cpsr, uint32_t spsr) {
 
 void dataab(uint32_t stackadress, uint32_t cpsr, uint32_t spsr) {
 
+}
+
+void irq(uint32_t stackadress, uint32_t cpsr, uint32_t spsr) {
+    print_interrupt(stackadress, cpsr, spsr, "TIMER INTERRUPT");
+    watchdog();
 }
--- src/include/Interrupt_Handler.h
+++ src/include/Interrupt_Handler.h
@@ -5,6 +5,10 @@
 #define BS_PRAK_INTERRUPT_HANDLER_H
 #include <stdint.h>
 
+void print_interrupt(uint32_t stackadress, uint32_t cpsr, uint32_t spsr, char* interrupt_name);
+
+void watchdog();
+
 void reset(uint32_t stackadress, uint32_t cpsr, uint32_t spsr);
 
 void undef(uint32_t stackadress, uint32_t cpsr, uint32_t spsr);
@@ -13,6 +17,8 @@ void prefab(uint32_t stackadress, uint32_t cpsr, uint32_t spsr);
 
 void dataab(uint32_t stackadress, uint32_t cpsr, uint32_t spsr);
 
+void irq(uint32_t stackadress, uint32_t cpsr, uint32_t spsr);
+
 struct timer {
     uint32_t LOAD;
     uint32_t VALUE;
@@ -20,7 +26,12 @@ struct timer {
     uint32_t IRQ_CLEAR_ACK;
     uint32_t unused[3];
     uint32_t PREDIVIDER;
+};
 
+struct en_basic_irq {
+    uint32_t IRQ_BASIC_PENDING;
+    uint32_t unused[5];
+    uint32_t EN_BASIC_IRQS;
 };
 
 #endif //BS_PRAK_INTERRUPT_HANDLER_H
