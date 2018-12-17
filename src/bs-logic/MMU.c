782e9b6818a4e0a9bedd5527ed6ade846ffe023e
lennard <lennard-boediger@t-online.de>
2018-12-17 15:28:21 +0100

    add mmu 1 zu 1

--- src/bs-logic/MMU.c
+++ src/bs-logic/MMU.c
@@ -6,6 +6,17 @@
 #define L1_ALLIGNMENT 16384
 #define MAX_ADDR 0x08000000 //128. MB
 #define FAULT_VALUE 0xfffffffc
+
+#define AP0_POS 10
+#define AP1_POS 11
+#define AP2_POS 12
+
+#define MMU_EN_POS 0
+#define ALIGNMENT_CHECK_EN_POS 1
+#define I_CHACHE_EN_POS 12
+#define C_CACHE_EN_POS 2
+
+
 //#define IO_REGION 0x3f000000 BIS 0x3f20000
 
 static uint32_t L1_table[L1_TABLE_SIZE]
@@ -18,21 +29,24 @@ uint32_t addr_to_index(uint32_t* addr) {
 //TODO BITS BEACHTEN
 void set_L1(){
     uint32_t i;
-    for(i = 0; i < max_addr_i-1; i++) {
-        L1_table[i] = (i << 20);
-        //setzt das 0. und 1. Bit auf Null, da Werte bei Init alle invalide
-        L1_table[i] = L1_table[i] & FAULT_VALUE;
-    }
-
     uint32_t max_addr_i = addr_to_index((uint32_t*) MAX_ADDR);
-    for(i; i < L1_TABLE_SIZE; i++) {
+    for(i = 0; i < L1_TABLE_SIZE; i++) {
+        L1_table[i] = (i << 20);        // 1 zu 1 mapping
+        L1_table[i] |= (1 << AP0_POS);  // AP = 011 Voller zugriff von allen (zum testen)
+        L1_table[i] |= (1 << AP1_POS);
+        L1_table[i] &= ~(1 << AP2_POS);
 
-        if (i > 0 && i < max_addr_i - 1) {  //user  kernel und stacks breich
+ // TODO: Weiter eingrenzen
+  /* if (i > 0 && i < max_addr_i - 1) {                             //user  kernel und stacks breich
             L1_table[i] = (i << 20);       //1 ZU 1 MAPPING
-        } else if (i == addr_to_index((uint32_t *)IO_REGION)) { // IO BEREICH
-
+        } else if (i == addr_to_index((uint32_t *)IO_REGION)) {     // IO BEREICH
         }
+        else{                                                       //Out of boud
+            L1_table[i] = L1_table[i] & FAULT_VALUE;
+        }
+*/
     }
+    kprintfln("L1_table[1]=00000000001 bit 15 = 0 bit 11-10=11 %x",L1_table[1]);
 }
 
 void init_L1_table(){
@@ -41,7 +55,11 @@ void init_L1_table(){
 
 void init_sctlr() {
     uint32_t sctlr = get_sctlr();
-    CCacheEn(2) = 0, ICacheEn(12) = 0, AlignmentCheckEnable(1) = 1, MmuEnable(0) = 1
+    //CCacheEn(2) = 0, ICacheEn(12) = 0, AlignmentCheckEnable(1) = 1, MmuEnable(0) = 1
+    sctlr &= ~(1 << C_CACHE_EN_POS);            //C_Cache aus
+    sctlr &= ~(1 << I_CHACHE_EN_POS);           //I_Cache aus
+    sctlr |= (1 << ALIGNMENT_CHECK_EN_POS);     //ALignment an
+    sctlr |= (1 << MMU_EN_POS);                 //MMU an
     set_sctlr(sctlr);
 }
 
@@ -52,7 +70,10 @@ void init_mmu() {
     init_L1_table();
 
     //TODO: WAS SOLL BEIM controlReg GESETZT WERDEN??? (Skriptseite: 1707ff)
-    //TODO: AccessFlagEnable(29) = 0?, TexRemapEnable(28) = 0?, CacheEnable(2) = 0?, AlignmentCheckEnable(1) = 1!, MmuEnable(0) = 1!
+   // TODO:/*nö AccessFlagEnable(29) = 0?, TexRemapEnable(28) = 0?,*/
+    // CacheEnable(2) = 0?, AlignmentCheckEnable(1) = 1! optional ,
+    // MmuEnable(0) = 1! Ichach bit (12 I)
+
     init_sctlr();
 
     //TODO: not_1_to_1_mapping
--- src/include/MMU-helper.h
+++ src/include/MMU-helper.h
@@ -2,10 +2,11 @@
 #define BS_PRAK_MMU_HELPER_H
 #include <stdint.h>
 
-
 void init_dacr(uint32_t dacr_value);
 void init_ttbcr(uint32_t ttbcr_value);
 void init_ttbr0(uint32_t* table_pointer);
 void init_sctlr();
+void set_sctlr(uint32_t sctlr);
+uint32_t get_sctlr();
 
 #endif //BS_PRAK_MMU_HELPER_H
